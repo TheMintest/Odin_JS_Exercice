6647507c5628cf4b74efe615836a055bdf16bbdc
Guillaume Macbook <g.dharcourt+github@posteo.net>
2022-02-06 19:44:51 +0100

    résolution exercice 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array) {
+    for (a = 0; a <= arguments.length; a++) {
+        for (i = 0; i <= array.length; i++) {
+            if (array[i] === arguments[a]) {
+                array.splice(i, 1);
+                break;
+            }
+        };
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
