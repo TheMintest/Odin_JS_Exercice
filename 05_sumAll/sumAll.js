4f8810d8821990a377ce8d22d6d82420da4c1857
Guillaume Macbook <g.dharcourt+github@posteo.net>
2022-02-06 21:12:47 +0100

    Résolution exercice 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(valueOne, valueTwo) {
+    sum = 0
+    let lowerValue = 0;
+    let higherValue = 0;
 
-};
+    if (valueOne < valueTwo) {
+        lowerValue = valueOne;
+        higherValue = valueTwo;
+    } else {
+        lowerValue = valueTwo;
+        higherValue = valueOne;
+    }
+
+    if ((typeof(valueTwo) === "number") && (typeof(valueOne) === "number")) {
+        if ((valueOne >= 0) && (valueTwo >= 0)) {
+            for (let i = lowerValue; i <= higherValue; i++) {
+                sum += i;
+            }
+        } else {
+            sum = "ERROR";
+        }
+    } else {
+        sum = "ERROR";
+    };
+    return sum;
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
