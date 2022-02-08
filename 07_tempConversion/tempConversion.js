ab2b9967e606e0328bcefb5c0553398d137e47d2
Guillaume Macbook <g.dharcourt+github@posteo.net>
2022-02-08 08:00:06 +0100

    résolution exercice 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    // [°C] = ([°F] − 32) × 5⁄9
+    let result = 0
+    result = (temp - 32) * (5 / 9);
+    return (Math.round(result * 10) / 10);
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+    //[°F] = [°C] × 9⁄5 + 32 
+    let result = 0
+    result = (temp * (9 / 5)) + 32;
+    return (Math.round(result * 10) / 10);
 
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 };
